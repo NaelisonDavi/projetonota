c1a1a719dff1585b655493e092dff4cbef544db0
NaelisonDavi <n.davi.c.oliveira@gmail.com>
2023-02-23 16:38:18 -0300

    terceira tentativa

--- projeto_nota.py
+++ projeto_nota.py
@@ -1,51 +1,45 @@
 import sys
 
-def materias(NOME_DO_ALUNO, NOME_DA_MATERIA, NOTA_DA_MATERIA):
-    while NOME_DO_ALUNO != 'sair':
-        NOME_DO_ALUNO = str(input('Digite o nome do aluno: '))
+with open('código da turma.txt', 'w') as arquivo:
 
-        if NOME_DO_ALUNO == 'sair':
-            break
+    def criador_de_dicionario(NOME_DO_ALUNO:int, NOME_DA_MATERIA:int, NOTA_DO_ALUNO:float):
+        while NOME_DO_ALUNO != 'sair': 
 
-        NOME_DA_MATERIA = str(input('Digite o nome da matéria: '))
+            NOME_DO_ALUNO = str(input('Digite o nome do aluno(a): \n'))
+            if NOME_DO_ALUNO == 'sair':
+                break
 
-        if NOME_DA_MATERIA == 'sair':
-            break
+            NOME_DA_MATERIA = str(input('Insira o nome da matéria: \n'))
+            if NOME_DA_MATERIA == 'sair':
+                break
 
-        NOTA_DA_MATERIA = float(input('Digite a nota da matéria: '))
+            NOTA_DO_ALUNO = float(input('Insira uma nota: \n'))
+            if NOTA_DO_ALUNO == 'sair':
+                break
 
-        if NOTA_DA_MATERIA == 'sair':
-            break
-            
-        MATERIA[NOME_DO_ALUNO] = [NOME_DA_MATERIA, NOTA_DA_MATERIA]
+            TODAS_AS_NOTAS = {'NOME_DO_ALUNO': {'NOME_DA_MATERIA': 'NOTA_DO_ALUNO'}}
 
-    for NOME_DO_ALUNO in MATERIA.keys():
+        return TODAS_AS_NOTAS
+        
+    def calcular_maior_nota(MAIOR_NOTA_ATUAL:float, NOTA_DO_ALUNO:float):
+        if NOTA_DO_ALUNO in TODAS_AS_NOTAS.items() > MAIOR_NOTA_ATUAL:
+            MAIOR_NOTA_ATUAL = NOTA_DO_ALUNO
+            return MAIOR_NOTA_ATUAL
+        return NOTA_DO_ALUNO
 
-        MAIOR_NOTA = maior_nota('maior_nota_atual', 'nova_nota')
-        if NOTA_DA_MATERIA == MAIOR_NOTA:
-            NOTA_DA_MATERIA = NOME_DO_ALUNO
+    def calcular_menor_nota(MENOR_NOTA_ATUAL:float, NOTA_DO_ALUNO:float):
+        if NOTA_DO_ALUNO in TODAS_AS_NOTAS.items() < MENOR_NOTA_ATUAL:
+            MENOR_NOTA_ATUAL = NOTA_DO_ALUNO
+            return MENOR_NOTA_ATUAL
+        return NOTA_DO_ALUNO
 
-        MENOR_NOTA_MATERIA = menor_nota('menor_nota_atual', 'nova_nota')
-        if NOTA_DA_MATERIA == MENOR_NOTA_MATERIA:
-            NOTA_DA_MATERIA = NOME_DO_ALUNO
+    if __name__ == '__main__':
 
-def maior_nota(maior_nota_atual:float, nova_nota:float):
-    if nova_nota > maior_nota_atual:
-        return nova_nota
-    return maior_nota_atual
+        CODIGO = str(input('Digite o código da turma(a): \n'))
+        MAIOR_NOTA_ATUAL = 0
+        MENOR_NOTA_ATUAL = sys.maxsize
+        TODAS_AS_NOTAS = dict()
 
-def menor_nota(menor_nota_atual:float, nova_nota:float):
-    if nova_nota < menor_nota_atual:
-        return nova_nota
-    return menor_nota_atual
-
-if __name__ == '__main__':
-
-    MAIOR_NOTA = 0
-    MENOR_NOTA = sys.maxsize
-    MATERIA = dict()
-
-materias('NOME_DO_ALUNO', 'NOME_DA_MATERIA', 'NOTA_DA_MATERIA')
-print(MATERIA)
-print(MAIOR_NOTA)
-print(MENOR_NOTA)
+criador_de_dicionario('NOME_DO_ALUNO', 'NOME_DA_MATERIA', 'NOTA_DO_ALUNO')
+calcular_maior_nota('MAIOR_NOTA_ATUAL', 'NOTA_DO_ALUNO')
+print(type(TODAS_AS_NOTAS))
